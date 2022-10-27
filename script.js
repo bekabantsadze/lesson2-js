f8375bd4c2bb2138514868a2b3a43cd4ceb0a4cf
Beka Bantasdze <thelataito@gmail.com>
2022-10-27 23:29:53 +0400

    lesson 2 commit

---
+++ script.js
@@ -0,0 +1,91 @@
+// let array = [2, -9, 5, 11, -30, 100, -8, -11, -4, 5, -6, 8]
+// let sum = 0;
+// let summedArr =[];
+
+// function sumUp() {
+//     for(i=0; i<array.length; i++){
+//         if(array[i]>0){
+//             summedArr.push(array[i])
+//             sum = sum + array[i]
+//         }
+//     }
+//     console.log(sum);
+// }
+
+// sumUp();
+
+// let array = [10, 50, 6, 7, 8, 11, 6, 3, 9]
+// let sum = 0;
+
+// function sumUp() {
+//     for(i=0; i<array.length; i++){
+//          if(array[i]>0){
+//             sum = sum + array[i]
+//          }
+    
+//     }
+//    console.log(sum);
+// }
+
+// sumUp()
+
+// let user = {
+//     firstname: 'giorgi',
+//     lastname: 'saakadze',
+//     age: 32,
+//     isloggedin: true
+//   }
+
+//   function username() {
+//         if(user.isloggedin === true){
+//             let userwraper = user.firstname+' '+user.lastname
+//             console.log(userwraper);
+//         }
+//   }
+// username()
+
+// let array = [2, 9, 5, 11, 30, 100, 8, 11, 4, 5, 6, 8]
+
+//     function arraypositive(x) {
+//         let max = Math.max(...x)
+
+//             console.log(max);
+      
+//     }
+//     arraypositive(array)
+
+// let array = [1,2,4,10,34,5,7,87]
+// let newarray = []
+
+// function between(){
+//     for(i=0; i<array.length; i++){
+//         if(array[i]>0 && array[i]<10){
+//             newarray.push(array[i])
+//         }
+//     }
+//     console.log(newarray);
+// }
+// between(newarray)
+
+// let array = [1,2,3,4,5,6,7,8,9,10]
+
+//     for(i=0; i<array.length; i++){
+//         if(array[i]===5){break}
+//         else{console.log(array[i]);}
+//     }
+    
+// let array = ['html', 'css', 'python', 'javascript', 'bootstrap'];
+
+// for(i=0; i<array.length; i++){
+//     if(array.length>4 && array[i].includes("av"))
+//     console.log(array[i]);
+
+// }
+
+// let array = ['html', 'css', 'python', 'javascript', 'bootstrap'];
+// function symb(item){
+//     if(item.length > 4 && item.includes("av"))
+//     console.log(item);
+
+// }
+// array.forEach(symb)
